6f4d4e6691a5d62bb99516e3c4290113b882fd90
Vitaliy <itvkip@yandex.ru>
2021-08-14 08:11:07 +0300

    Created styles for input component

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,7 +3,10 @@ import style from './input.module.css';
 
 function Input(){
 	return(
-		<input type='text' className={style.modalBtn}/>
+		<div className={style.inputBlock}>
+			<label htmlFor='inp1'>Имя<sup>*</sup></label>
+			<input id='inp1' type='text' className={style.modalBtn} disabled placeholder='Имя'/>
+		</div>
 	)
 }
 
